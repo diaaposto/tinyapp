2883d667cfab2e712452c5449f4b6b9a9bad7124
Dia Apostolakis <dia.apostolakis@icloud.com>
2018-08-07 21:30:01 +0000

    first commit

---
+++ express_server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+// When sending variables to an EJS template, you need to send them inside an object, 
+// even if you are only sending one variable. 
+// This is so you can use the key of that variable (in the above case the key is urls) 
+// to access the data within your template.
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+    res.end("<html><body>Hello <b>World</b></body></html>\n");
+});  
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
